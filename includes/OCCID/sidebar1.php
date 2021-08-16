3795de80c187c03a642c4a29d3c387f1e52d88c6
TazarChriss <77143206+tazarchriss@users.noreply.github.com>
2021-08-16 08:26:55 +0300

    added OCCID sidebar

---
+++ includes/OCCID/sidebar1.php
@@ -0,0 +1,59 @@
+ <!--sidebar start-->
+ <aside>
+     <div id="sidebar" class="nav-collapse ">
+         <!-- sidebar menu start-->
+         <ul class="sidebar-menu">
+             <li class="active">
+                 <a class="" href="<?php echo BASE_URL . '/OCCID/cases/index.php' ?>">
+                     <i class="icon_house_alt"></i>
+                     <span>Dashboard</span>
+                 </a>
+             </li>
+             <li class="sub-menu">
+                 <a href="javascript:;" class="">
+                     <i class="icon_document_alt"></i>
+                     <span>Cases</span>
+                     <span class="menu-arrow arrow_carrot-right"></span>
+                 </a>
+                 <ul class="sub">
+                     <li><a class="" href="<?php echo BASE_URL . '/OCCID/cases/index.php' ?>">View Cases</a></li>
+                     
+                 </ul>
+             </li>
+             <li>
+                 <a class="" href="<?php echo BASE_URL . '/OCCID/officers/index.php' ?>">
+                     <i class="icon_genius"></i>
+                     <span>Officers</span>
+                 </a>
+             </li>
+            
+             <li class="sub-menu">
+                 <a href="javascript:;" class="">
+                 <i class="icon_genius"></i>
+                     <span>Investigation</span>
+                     <span class="menu-arrow arrow_carrot-right"></span>
+                 </a>
+                 <ul class="sub">
+                     <li><a class="" href="<?php echo BASE_URL . '/OCCID/investigation/index.php' ?>">Investigation</a></li>
+                    
+                     <li><a class="" href="<?php echo BASE_URL . '/OCCID/investigation/evidences.php' ?>"><span>Evidences</span></a></li>
+                 </ul>
+             </li>
+
+             <li class="sub-menu">
+                 <a href="javascript:;" class="">
+                     <i class="icon_documents_alt"></i>
+                     <span>Pages</span>
+                     <span class="menu-arrow arrow_carrot-right"></span>
+                 </a>
+                 <ul class="sub">
+                     <li><a class="" href="<?php echo BASE_URL . '/OCCID/profile/profile.php' ?>">Profile</a></li>
+                     <li><a class="" href="<?php echo BASE_URL . '/logout.php' ?>"><span>Logout</span></a></li>
+                 </ul>
+             </li>
+
+         </ul>
+         <!-- sidebar menu end-->
+     </div>
+ </aside>
+ <!--sidebar end-->
